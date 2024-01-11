fc4a5d8fa483b8a8c5e9e63e5ba4052be6f61eb8
HttpMarco <mirco.lindenau@gmx.de>
2024-01-11 22:25:50 +0100

    Change readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,55 @@
-import net.bytemc.gradle.extension.nexus.NexusRepositories
-
 plugins {
-    id("java")
-    id("net.bytemc.gradle.structure") version "1.0.0-20231220.135012-7"
+    `java-library`
+    `maven-publish`
 }
 
 group = "net.bytemc.pvp"
-version = "1.0-SNAPSHOT"
-
-bytemc {
-
-    jarName =  "pvp"
-
-    buildShadowJar()
+version = "1.0.0-SNAPSHOT"
 
 
-    nexus {
-        repositories(NexusRepositories.BYTEMC_ALL)
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
-    dependencies {
-        compileOnly(libs.lombok)
-        annotationProcessor(libs.lombok)
-        implementation(libs.gson)
-        compileOnly("net.bytemc:minestom:1.4.3")
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://nexus.bytemc.de/repository/maven-public/")
     }
 }
+
+dependencies {
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    implementation(libs.gson)
+    compileOnly("net.bytemc:minestom:1.4.7-SNAPSHOT")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            this.groupId = project.group.toString()
+            this.artifactId = project.name
+            this.version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            name = "bytemc"
+            url = uri("https://nexus.bytemc.de/repository/maven-public/")
+            credentials {
+                username = System.getenv("BYTEMC_REPO_USER")
+                password = System.getenv("BYTEMC_REPO_PASSWORD")
+            }
+        }
+    }
+}
+
--- readme.md
+++ readme.md
@@ -6,6 +6,36 @@ MinestomPvp is outdated and not working with latest Minestom version.
 
 
 ### Startring
+#### Repository
+
+Maven
+```xml
+<repository>
+    <id>public-bytemc</id>
+    <url>https://nexus.bytemc.de/repository/maven-public/</url>
+</repository>
+```
+
+Gradle (Kotlin DSL)
+```kotlin
+maven {
+    url = uri("https://nexus.bytemc.de/repository/maven-public/")
+}
+```
+
+####  Dependency
+Maven
+```xml
+<dependency>
+    <groupId>net.bytemc.pvp</groupId>
+    <artifactId>pvp</artifactId>
+    <version>VERSION</version>
+</dependency>
+```
+Gradle (Kotlin DSL)
+```kotlin
+implementation("net.bytemc.pvp:pvp:VERSION")
+```
 ***
 ```java
  MinecraftServer.getGlobalEventHandler().addChild(PvpBuilder.of(PvpMode.LEGACY).build());
